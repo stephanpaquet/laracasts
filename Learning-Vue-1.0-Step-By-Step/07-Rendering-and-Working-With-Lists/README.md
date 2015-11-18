e4149fcc9a54b7c541c5a41190dcbdca0240f2a1
spaquet <stephan.paquet@duproprio.com>
2015-11-18 15:38:12 -0500

    Add lesson 7

---
+++ Learning-Vue-1.0-Step-By-Step/07-Rendering-and-Working-With-Lists/README.md
@@ -0,0 +1,4 @@
+# Rendering and Working With Lists
+Published on Nov. 17th 2015
+
+Let's learn how to filter through and render lists in this lesson.
---
+++ Learning-Vue-1.0-Step-By-Step/07-Rendering-and-Working-With-Lists/app.js
@@ -0,0 +1,20 @@
+/*global Vue, window*/
+
+var appVue = new Vue({
+    el: '#app',
+
+    data: {
+        tasks: [
+            {body:  'Go to the store', completed: false },
+            {body:  'Go to the bank', completed: true },
+            {body:  'Go to the doctor', completed: false }
+        ]
+    },
+
+    methods: {
+        toggleCompletedFor: function (task) {
+            task.completed = ! task.completed;
+        }
+    }
+
+});
