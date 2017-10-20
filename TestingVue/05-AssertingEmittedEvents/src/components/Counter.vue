8e4d09fe98b8d996d56758400f23afbf9f75586e
Stéphan Paquet <stephan.paquet@imarcom.net>
2017-10-20 14:23:40 -0400

    Lesson 5 - before

---
+++ TestingVue/05-AssertingEmittedEvents/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <span class="count">{{ count }}</span>
+        <button class="increment" @click="increment">
+            Increment
+        </button>
+        <button class="decrement" @click="decrement" v-show="count > 0">
+            Decrement
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                count: 0
+            };
+        },
+        methods: {
+            increment () {
+                this.count += 1;
+            },
+            decrement () {
+                this.count = (this.count > 0) ? this.count -= 1 : 0;
+            }
+        }
+    }
+</script>
---
+++ TestingVue/05-AssertingEmittedEvents/src/components/Reminders.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="text" class="new-reminder" v-model="newReminder">
+        <button @click="add">
+            Add reminder
+        </button>
+
+        <ul v-if="reminders.length">
+            <li v-for="reminder in reminders" :key="reminder">
+                {{ reminder }}
+                <input type="text" class="remove" @click="remove(reminder)">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                newReminder: '',
+                reminders: []
+            }
+        },
+        methods: {
+            add () {
+                this.reminders.push(this.newReminder);
+
+                this.newReminder = '';
+            },
+            remove (reminder) {
+                this.reminders.splice(reminder, 1);
+                console.log(reminder);
+
+            }
+        }
+    };
+</script>
